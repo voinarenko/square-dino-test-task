8b1d7885b54d42f79923fa235161f7fb7a503001
Sergei Voinarenko <voynarenko@gmail.com>
2024-07-15 12:22:19 +0300

    Enemy health bar added.

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Enemy
 {
     public class EnemyHealth : MonoBehaviour
     {
+        [SerializeField] private Image _healthImage;
         public event Action Changed;
         public event Action Dead;
 
@@ -26,10 +28,19 @@ namespace Enemy
 
         private int _currentHealth;
         
+        private void Awake() => 
+            Changed += OnHealthChanged;
+        
+        private void OnDestroy() => 
+            Changed -= OnHealthChanged;
+        
         public void ResetHealth() => 
             CurrentHealth = MaxHealth;
         
         public void TakeDamage(int amount) =>
             CurrentHealth -= amount;
+        
+        private void OnHealthChanged() => 
+            _healthImage.fillAmount = (float)CurrentHealth / MaxHealth;
     }
 }
